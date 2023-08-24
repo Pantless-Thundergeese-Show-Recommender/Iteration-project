faaa4bbd7a2e7eaaefb9667cc05f23c69e15ba5b
Wanlu D <46799027+WanluD@users.noreply.github.com>
2023-08-24 12:43:44 -0400

    tested for sign up page with 1 fail, before lunch break

---
+++ __tests__/reactForm.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { SignUpForm } from '../client/components/TestDisplay.jsx';
+
+describe('SignupForm', () => {
+  it('renders without crashing', () => {
+    expect(() => render(<SignUpForm />)).not.toThrow();
+  });
+
+  it('renders a first name text input', () => {
+    render(<SignUpForm />);
+    expect(screen.getByLabelText('Username')).toBeInTheDocument();
+  });
+
+  it('renders a last name text input', () => {
+    render(<SignUpForm />);
+    expect(screen.getByLabelText('Password')).toBeInTheDocument()
+  })
+
+  it('renders an error message for each of the inputs if none of them are filled out and the user submits the form', () => {
+    render(<SignUpForm />)
+    fireEvent.click(screen.getByText('Sign up'))
+    
+    expect(screen.getByTestId('Username field is required')).toBeInTheDocument();
+    expect(screen.getByTestId('Password field is required')).toBeInTheDocument();
+  })
+
+});
--- __tests__/reactTest.js
+++ __tests__/reactTest.js
@@ -3,11 +3,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
-import regeneratorRuntime from 'regenerator-runtime'
+import regeneratorRuntime from 'regenerator-runtime';
 // import { configureStore } from '@reduxjs/toolkit';
 // const { render, screen, waitFor} = require('@testing-library/react');
 // const regenerator = require('regenerator-runtime');
-
 
 // we are using Jest here -- you could use other testing frameworks with RTL or use RTL on its own, but here we get the best of both worlds
 
@@ -24,50 +23,43 @@ import configureStore from 'redux-mock-store';
 
 const mockStore = configureStore([]);
 
-
-
 describe('Testing React Components', () => {
+  test('Display Input component', async () => {
+    //render(<Input store={store} />)
 
+    const initialState = {
+      shows: [],
+      loading: false,
+      error: null,
+      showAddButton: true,
+      showDeleteButton: false,
+    };
 
-    test('Display Input component', async () => {
-        //render(<Input store={store} />)
+    const store = mockStore(initialState);
 
-        const initialState = {
-            shows: [],
-            loading: false,
-            error: null,
-            showAddButton: true,
-            showDeleteButton: false
-          }
-        
-          const store = mockStore(initialState);
+    render(
+      <Provider store={store}>
+        <Input />
+      </Provider>
+    );
 
-          render(<Provider store={store}>
-                    <Input />
-                </Provider>)
+    const buttons = await screen.findAllByRole('button');
+    expect(buttons.length).toBe(1);
+    //expect(buttons[0]).toHaveTextContent(' submit ');
 
-        
-    
-    
-        const buttons = await screen.findAllByRole('button')
-        expect(buttons.length).toBe(2);
-        //expect(buttons[0]).toHaveTextContent(' submit ');
-    
-        // const inputs = await screen.findAllByRole('input')
-        // expect(buttons.length).toBe(4);
-        
-    })
+    // const inputs = await screen.findAllByRole('input')
+    // expect(buttons.length).toBe(4);
+  });
 
-    // test('test testDisplay component', async() => {
-    //     console.log('before')
-    //     render(<TestDisplay />)
+  // test('test testDisplay component', async() => {
+  //     console.log('before')
+  //     render(<TestDisplay />)
 
-    //     console.log('after')
+  //     console.log('after')
 
-    //     const headings = await screen.findAllByRole('heading');
+  //     const headings = await screen.findAllByRole('heading');
 
-    //     expect(headings[0]).toHaveTextContent('Hello');
+  //     expect(headings[0]).toHaveTextContent('Hello');
 
-
-    // })
-})
+  // })
+});
